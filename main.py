885e0dd89dfbd1217130b3247ee928c1f5cfa231
AdrianN001 <gogos.adrian2006@gmail.com>
2021-11-30 20:27:50 +0100

    idk

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import requests
 import json
 
-# https://community.w2g.tv/t/watch2gether-api-documentation/133767
-header1= {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-}
+def roomCreate(apiKey:str):
+    header1= {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
 
-r = requests.post("https://w2g.tv/rooms/create.json",headers = header1, data =json.dumps({"w2g_api_key": "gqv3cx5qiier6r5kum9nhhomq6uqw614xqdq4itc58wkrh562n0gitxs768audjk","share": "https://www.youtube.com/watch?v=8Wdp35Z-fRs","bg_color": "#00ff00","bg_opacity": 50 }))
+    r = requests.post("https://w2g.tv/rooms/create.json",headers = header1, data =json.dumps({"w2g_api_key": apiKey,"share": "https://www.youtube.com/watch?v=8Wdp35Z-fRs","bg_color": "#00ff00","bg_opacity": 50 }))
 
-x = json.loads(r.text)
-print(x['streamkey'])
+    x = json.loads(r.text)
+    print(f' A szobád : \n https://w2g.tv/rooms/{x["streamkey"]}')
+
+
+
+
+
+if __name__ == '__main__':
+    roomCreate('gqv3cx5qiier6r5kum9nhhomq6uqw614xqdq4itc58wkrh562n0gitxs768audjk')
