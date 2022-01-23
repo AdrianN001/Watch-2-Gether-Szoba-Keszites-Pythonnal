92bf939d90d688937b35350023943e85f1af256e
AdrianN001 <gogos.adrian2006@gmail.com>
2022-01-23 20:58:23 +0100

    renamed it, and made it to return only  the link

---
+++ w2g.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def room_create(api_key:str) -> str:
+    header1= {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    r = requests.post("https://w2g.tv/rooms/create.json",
+                        headers = header1, 
+                        data =json.dumps(
+                                {"w2g_api_key": api_key,
+                                "share": "https://www.youtube.com/watch?v=8Wdp35Z-fRs",
+                                "bg_color": "#00ff00",
+                                "bg_opacity": 50 }
+                                        )
+                      )
+
+    x = json.loads(r.text)
+    return f'https://w2g.tv/rooms/{x["streamkey"]}'
+
+
+
+
+
+if __name__ == '__main__':
+    print(room_create('gqv3cx5qiier6r5kum9nhhomq6uqw614xqdq4itc58wkrh562n0gitxs768audjk'))
